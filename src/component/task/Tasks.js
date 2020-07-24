d4a9e164d9e3a065e9431a24dfa5b08666127478
JaiRaga <raga.j.santhosh@gmail.com>
2020-07-24 09:58:49 +0530

    displays all tasks

---
+++ src/component/task/Tasks.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+import TaskItem from "./TaskItem";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  spacing: {
+    marginTop: 30
+  },
+  taskitem: {
+    margin: 15
+  }
+}));
+
+const Tasks = () => {
+  const classes = useStyles();
+  return (
+    <Grid
+      container
+      item
+      direction='column'
+      justify='center'
+      alignItems='center'
+      wrap='nowrap'
+      className={classes.spacing}>
+      <Grid item className={classes.taskitem}>
+        <TaskItem />
+      </Grid>
+      <Grid item className={classes.taskitem}>
+        <TaskItem />
+      </Grid>
+      <Grid item className={classes.taskitem}>
+        <TaskItem />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Tasks;
