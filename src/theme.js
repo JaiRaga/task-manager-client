3015b1cf06c20b156c3bff8df25e4d6e27a7f864
JaiRaga <raga.j.santhosh@gmail.com>
2020-07-24 10:03:03 +0530

    created theme file for material ui

---
+++ src/theme.js
@@ -0,0 +1,23 @@
+import { createMuiTheme } from "@material-ui/core";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#2196f3"
+    },
+    secondary: {
+      main: "#19857b"
+    },
+    error: {
+      main: "#f00"
+    },
+    background: {
+      default: "#fff"
+    },
+    workspace: {
+      main: "#f3e1e1"
+    }
+  }
+});
+
+export default theme;
