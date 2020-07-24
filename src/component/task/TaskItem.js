608aa030fc72c4f68727d029a70cbff1c555d60c
JaiRaga <raga.j.santhosh@gmail.com>
2020-07-24 11:18:46 +0530

    modified grid layout and added dropdown button

--- src/component/task/TaskItem.js
+++ src/component/task/TaskItem.js
@@ -1,26 +1,40 @@
 import React from "react";
-import { Grid, Paper } from "@material-ui/core";
+import { Grid, Paper, IconButton } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 
 const useStyles = makeStyles((theme) => ({
   taskitem: {
-    margin: 15
+    // margin: 15
   },
   paper: {
-    margin: 15,
-    padding: "25px 45px"
+    // margin: 15,
+    paddingTop: "25px",
+    paddingBottom: "25px",
+    paddingLeft: "45px",
+    paddingRight: "15px",
+    backgroundColor: "#2f2519",
+    color: "#f4f6ff"
   }
 }));
 
 const TaskItem = () => {
   const classes = useStyles();
   return (
-    // <Grid item className={classes.taskitem}>
     <Paper className={classes.paper} elevation={4}>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto nobis,
-      dicta laboriosam suscipit eius voluptatibus deleniti nihil hic a ratione.
+      <Grid container wrap='nowrap' item className={classes.taskitem}>
+        <Grid item>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto nobis,
+          dicta laboriosam suscipit eius voluptatibus deleniti nihil hic a
+          ratione.
+        </Grid>
+        <Grid item>
+          <IconButton aria-label='edit/delete' color='inherit'>
+            <ArrowDropDownIcon />
+          </IconButton>
+        </Grid>
+      </Grid>
     </Paper>
-    // </Grid>
   );
 };
 
